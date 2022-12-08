85ed9a84a78b356529e6e0789065ea06d28863aa
OjasweeU <119649108+OjasweeU@users.noreply.github.com>
2022-12-08 11:57:24 +0530

    Add files via upload

---
+++ SampQ10.java
@@ -0,0 +1,15 @@
+//Convert decimal to binary
+package practice1;
+
+public class SampQ10 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		   int decimal = 91; // decimal number
+		    String binary = Integer.toBinaryString(decimal);   // convert decimal to binary
+		    System.out.println(decimal + " in decimal = " + binary + " in binary.");
+	}
+
+}
---
+++ SampQ9.java
@@ -0,0 +1,18 @@
+//Convert string to integer
+package practice1;
+
+public class SampQ9 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		 String strTest = "200";
+		    //Convert the String to Integer using Integer.valueOf
+		    int iT = Integer.valueOf(strTest);
+		    System.out.println("Actual String:"+ strTest);
+		    System.out.println("Converted to Int:" + iT);
+
+	}
+
+}
