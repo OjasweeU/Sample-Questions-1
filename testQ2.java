125280893041309309a981da82bc260e6ac4426a
OjasweeU <119649108+OjasweeU@users.noreply.github.com>
2022-12-08 11:51:16 +0530

    Add files via upload

---
+++ testQ2.java
@@ -0,0 +1,12 @@
+package SAmpQ2;
+
+public class testQ2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+				System.out.println(rev.reverseString("my name is khan"));  
+			    System.out.println(rev.reverseString("I am sonoo jaiswal"));  
+	}
+
+}
